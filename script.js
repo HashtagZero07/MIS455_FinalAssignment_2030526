5c0ff38eb3d7d021feddb7da19e7274962aad1d3
HashtagZero07 <2030526@iub.edu.bd>
2023-12-20 20:26:27 +0600

    4th commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function searchCountry(){
 
     var search=document.getElementById("search").value; 
    
-    var url=`https://restcountries.com/v3.1/all=${search}` ;
+    var url=`https://restcountries.com/v3.1/name/${search}` ;
    
     fetch (url)
         .then (res => res.json())
@@ -13,10 +13,39 @@ function searchCountry(){
 
 
 
-function displayCountryData(data){
+function displayCountryData(data) {
+    var countryDataDisplay = document.getElementById("displayContainer");
+    countryDataDisplay.innerHTML = "";
 
-    var countryData=data;
-    console.log(countryData);
+    if (data.length > 0) {
+        var country = data[0];
 
+        var flagImage = document.createElement("img");
+        flagImage.src = country.flags.svg;
+
+        var nameParagraph = document.createElement("p");
+        nameParagraph.textContent = "NAME: " + country.name.common;
+
+        var populationParagraph = document.createElement("p");
+        populationParagraph.textContent = "POPULATION: " + country.population;
+
+        var languageParagraph = document.createElement("p");
+        languageParagraph.textContent = "LANGUAGE: " + country.languages[0];
+
+        var landAreaParagraph = document.createElement("p");
+        landAreaParagraph.textContent = "LAND AREA: " + country.area + " square kilometers";
+
+        countryDataDisplay.appendChild(flagImage);
+        countryDataDisplay.appendChild(nameParagraph);
+        countryDataDisplay.appendChild(populationParagraph);
+        countryDataDisplay.appendChild(languageParagraph);
+        countryDataDisplay.appendChild(landAreaParagraph);
+        
+    } else {
+
+        var notFoundParagraph = document.createElement("p");
+        notFoundParagraph.textContent = "Country not found.";
+        countryDataDisplay.appendChild(notFoundParagraph);
+    }
 }
    
